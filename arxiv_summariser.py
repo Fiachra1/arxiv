4fc4a7f3d8e391f77a2e3c3ef211ca8c786c7267
Fiachra Merwick <fmerwick@Fiachras-MacBook-Air.local>
2023-12-12 00:15:42 +0000

    Changed query to lSTM

--- arxiv_summariser.py
+++ arxiv_summariser.py
@@ -6,7 +6,7 @@ client = arxiv.Client()
 
 # Search for the 10 most recent articles matching the keyword "quantum."
 search = arxiv.Search(
-    query="cat:cs.AI AND ti:quantum",
+    query="cat:cs.AI AND ti:lstm",
     max_results=100,
     sort_by=arxiv.SortCriterion.SubmittedDate,
 )
@@ -21,15 +21,3 @@ for r in client.results(search):
 # ...or exhaust it into a list. Careful: this is slow for large results sets.
 # all_results = list(results)
 # print([r.title for r in all_results])
-
-# For advanced query syntax documentation, see the arXiv API User Manual:
-# https://arxiv.org/help/api/user-manual#query_details
-# search = arxiv.Search(query="au:del_maestro AND ti:checkerboard")
-# first_result = next(client.results(search))
-# print(first_result)
-
-# Search for the paper with ID "1605.08386v1"
-# search_by_id = arxiv.Search(id_list=["1605.08386v1"])
-# Reuse client to fetch the paper, then print its title.
-# first_result = next(client.results(search))
-# print(first_result.title)
