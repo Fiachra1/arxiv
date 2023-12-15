b4eaf1969516cd94e6eaf2cdd3e1b1d3d32f412b
Fiachra Merwick <fmerwick@Fiachras-MacBook-Air.local>
2023-12-15 18:24:47 +0000

    Email formatting updated

--- arxiv_summariser.py
+++ arxiv_summariser.py
@@ -23,33 +23,57 @@ def mail_msg(message):
     s.quit()
     
 client = OpenAI()
-
 # Construct the default API client.
 arxiv_client = arxiv.Client()
 
 # Search for the 10 most recent articles matching the keyword "quantum."
 search = arxiv.Search(
-    query="cat:cs.AI AND ti:quantum",
-    max_results=100,
-    sort_by=arxiv.SortCriterion.SubmittedDate,
+    #query="cat:cs.AI AND ti:quantum",
+    query="ti:AI",
+    max_results=25,
+    sort_by=arxiv.SortCriterion.SubmittedDate
 )
 results = arxiv_client.results(search)
 
 all_results = list(results)
 
-contents =[]
-for i in range(len(all_results)):
-    if all_results[i].published.date() > datetime.date.today() + datetime.timedelta(days=-7):
-        completion = client.chat.completions.create(
+if len(all_results)>0:
+    contents =[]
+    for i in range(len(all_results)):
+        if all_results[i].published.date() >= datetime.date.today() + datetime.timedelta(days=-2):
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a quantum AI researcher, skilled in explaining complex programming concepts for a general audience"},
+                    {"role": "user", "content": f"Using this format: 'Applicability to Quantum Deep Learning Research: ' Indicate whether the paper titled {all_results[i].title} th abstract {all_results[i].summary}\
+                    has a High, Medium or Low applicability. Then summarise the paper in 3 main concise bullet points. Ensure everything is formatted nicely"}
+                ]
+                )
+    
+            contents.append('Paper Title: ' + all_results[i].title + ' \n Link to paper: ' + all_results[i].entry_id)
+            contents.append('Arxiv Category: ' + all_results[i].primary_category)
+            contents.append(completion.choices[0].message.content)
+        else:
+            print("No Papers to review")
+    
+    bullet_summaries = '\n\n'.join(contents)
+
+    if len(contents) > 0:
+        summary_data = bullet_summaries[:15000] if len(bullet_summaries) > 15000 else bullet_summaries
+        summary_para = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are an ai researcher, skilled in explaining complex programming concepts for a general audience"},
-                    {"role": "user", "content": f"Summarise this abstract from the paper titled {all_results[i].title} in 3 main bullet points, keep the points concise and before the bullet points state whether the paper has a High, Medium or Low applicability to Quantum Deep Learning research in time-series analysis, then add two empty lines: {all_results[i].summary}"}
-            ]
-            )
+                {"role": "system", "content": "You are an eloquent ai researcher, skilled in explaining complex concepts for a general audience"},
+                {"role": "user", "content": f"Summarise in a short paragraph, a general overview of what the papers have investigate, highlighting the most promising papers that relate to deep learning using quantum computing. Here is the text: {summary_data}"}])
+        
+        print(summary_para.choices[0].message.content)
+        message_to_send =f'{i+1} Papers Reviewed Today \n\n\n'+summary_para.choices[0].message.content+'\n\n\n\n'+bullet_summaries 
 
-        contents.append(all_results[i].title + ' : ' + all_results[i].entry_id)
-        contents.append(all_results[i].primary_category)
-        contents.append(completion.choices[0].message.content)
-
-mail_msg('\n\n'.join(contents))
+        try:
+            mail_msg(message_to_send)   
+        except NameError:
+            print("No mail today")
+        else:
+            print("You've got mail!")
+else:
+    print("No papers returned from Arxiv")
