9f1e998aea355e2b11f5fce88bbadb6e8f6dedd2
Fiachra Merwick <fmerwick@Fiachras-MacBook-Air.local>
2023-12-17 23:12:45 +0000

    mail formatting and messages working as expected

--- arxiv_summariser.py
+++ arxiv_summariser.py
@@ -28,7 +28,7 @@ arxiv_client = arxiv.Client()
 
 # Search for the 10 most recent articles matching the keyword "quantum."
 search = arxiv.Search(
-    query="quantum",
+    query="cat:cs.AI AND ti:quantum",
     max_results=20,
     sort_by=arxiv.SortCriterion.SubmittedDate
 )
@@ -38,8 +38,9 @@ all_results = list(results)
 
 if len(all_results)>0:
     contents =[]
+    j=0
     for i in range(len(all_results)):
-        if all_results[i].published.date() >= datetime.date.today() + datetime.timedelta(days=-2):
+        if all_results[i].published.date() >= datetime.date.today() + datetime.timedelta(days=-7):
             completion = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
@@ -49,15 +50,17 @@ if len(all_results)>0:
                 ]
                 )
     
-            contents.append('Paper Title: ' + all_results[i].title + ' \n Link to paper: ' + all_results[i].entry_id)
+            contents.append('\n\n\n\nPaper Title: ' + all_results[i].title + ' \n Link to paper: ' + all_results[i].entry_id)
             contents.append('Arxiv Category: ' + all_results[i].primary_category)
             contents.append(completion.choices[0].message.content)
             print(f"Paper {i} titled {all_results[i].title} reviewed as it was published on {all_results[i].published.date()}")
             print(completion.choices[0].message.role)
+            j += 1
         else:
             print(f"Paper not reviewed as it was published on {all_results[i].published.date()}")
     
     bullet_summaries = '\n\n'.join(contents)
+    print(len(bullet_summaries))
 
     if len(contents) > 0:
         summary_data = bullet_summaries[:15000] if len(bullet_summaries) > 15000 else bullet_summaries
@@ -65,11 +68,9 @@ if len(all_results)>0:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are an eloquent ai researcher, skilled in explaining complex concepts for a general audience"},
-                {"role": "user", "content": f"Summarise in a short paragraph, a general overview of what the papers have investigate, highlighting the most promising papers that relate to deep learning using quantum computing. Here is the text: {summary_data}"}])
+                {"role": "user", "content": f"The following text is a summarises multiple papers from arxiv: {summary_data} . Provide a brief summary of the general content investigated in the papers and the main paper I should focus on when considering research in quantum deep learning of time series data"}])
         
-        print(summary_para.choices[0].finish_reason)
-        print(summary_para.choices[0].message.role)
-        message_to_send =f'{i+1} Papers Reviewed Today \n\n\n'+summary_para.choices[0].message.content+'\n\n\n\n'+bullet_summaries 
+        message_to_send =(f'{j} Papers Reviewed Today \n\n\n\tSummary of Research Content\n\n'+summary_para.choices[0].message.content+'\n\n\n\n\tResearch papers:'+bullet_summaries)
 
         try:
             mail_msg(message_to_send)   
